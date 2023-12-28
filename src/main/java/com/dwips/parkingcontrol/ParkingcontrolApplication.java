1deb022608688054c77104d487c283f838c3bcec
Kim Jae-Won <okhi1@naver.com>
2023-12-28 18:04:42 +0900

    Initial commit

---
+++ src/main/java/com/dwips/parkingcontrol/ParkingcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.dwips.parkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dwips/parkingcontrol/ParkingcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwips.parkingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
