c2658b8d31b8f57acecc4e83a3caee74f404c14f
Kim Jae-Won <okhi1@naver.com>
2024-02-08 03:58:26 +0900

    [Frontend Admin, API] cros 에러 에방, 통신 테스트

--- src/main/frontend-admin/src/App.js
+++ src/main/frontend-admin/src/App.js
@@ -1,9 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
+
 function App() {
+
+  const [data,setData] = useState("");
+
+  useEffect(()=>{
+    axios.post("http://localhost:20000/api/v1/calculate",{
+      "IOTYPE": "C",
+      "sitenum": 1,
+      "groupnum": 1,
+      "devicenum": 402,
+      "carnum": "808너1303"
+    })
+    .then(res => {
+      console.log(res.data);
+      setData(JSON.stringify(res.data));
+    })
+    .catch(error => console.log(error));
+  },[]);
+
+
+
   return (
     <div className="App">
+      <div>받아온 값 : {data}</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
